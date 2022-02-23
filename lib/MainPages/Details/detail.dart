1ca7defea73c40bed85872236a7fe384bff106d9
elsone <elsanal1995@gmail.com>
2022-02-23 11:13:31 +0800

    details

--- lib/MainPages/Details/detail.dart
+++ lib/MainPages/Details/detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:openshop/Decorations/text_deco.dart';
 import 'package:openshop/MainPages/Details/detail_body.dart';
 import 'package:openshop/MainPages/Details/image_slider.dart';
 
@@ -18,10 +20,54 @@ class _DetailState extends State<Detail> {
       body: SizedBox(
         height: height,
         width: width,
-        child: ListView(children: [
-          imageSwiper(5, width, height),
-          detailBody()
-        ],),),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,left: 0,right: 0,
+              bottom: ScreenUtil().setHeight(200),
+              child: ListView(children: [
+                imageSwiper(5, width, height),
+                detailBody()
+              ],),
+            ),
+            Positioned(
+              bottom: 0,left: 0,right: 0,
+                child: Column(children: [
+                  _bottomOrder(),
+                  _bottomOrder()
+                ],)
+            )
+          ],
+        ),),
     );
   }
+
+  Widget _bottomOrder(){
+    return Container(
+      color: Colors.black,
+      padding: EdgeInsets.only(
+        top: ScreenUtil().setHeight(15),
+        bottom: ScreenUtil().setHeight(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+        _button("Chat now"),
+        _button("Add to Cart"),
+        _button("Order now")
+      ],),
+    );
+  }
+
+  Widget _button(String label){
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(ScreenUtil().setHeight(20)),
+        decoration:const BoxDecoration(
+            color: Colors.deepOrangeAccent,
+            borderRadius: BorderRadius.all(Radius.circular(25))
+        ),
+        child: Text(label,style: style,),),);
+  }
+
 }
--- lib/MainPages/Details/detail_body.dart
+++ lib/MainPages/Details/detail_body.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:openshop/Decorations/text_deco.dart';
 
 Widget detailBody(){
-  return Column(children:[
-    Text("une tres bonne chaussures. C'est la vraie qualitée. "
-        "il ya differentes tailles pour enfants et adultes, "
-        "femme et homme",style: style.copyWith(color: Colors.black),)
-  ],);
+  return Container(
+    padding: EdgeInsets.all(ScreenUtil().setWidth(15)),
+    child: Column(children:[
+      Text.rich(TextSpan(
+          text: "Price : ",
+          spellOut: true,
+          style: const TextStyle(
+              fontWeight: FontWeight.w400
+          ),
+          children: <TextSpan>[
+            TextSpan(
+              text: '\$8.99',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                decoration: TextDecoration.lineThrough,
+              ),
+            ),
+            const TextSpan(
+                text: '  \$3.99',
+                style: TextStyle(color: Colors.red,)
+            ),
+            const TextSpan(text: '\nRate: ',),
+            const TextSpan(
+                text: '★★★☆☆',
+                style: TextStyle(color: Colors.amber)),
+            const TextSpan(text: '\nSold : 943832',),
+            const TextSpan(
+                text: "\nune tres bonne chaussures. C'est la vraie qualitée. "
+                    "il ya differentes tailles pour enfants et adultes, "
+                    "femme et homme. une tres bonne chaussures. C'est la vraie qualitée. "
+                    "il ya differentes tailles pour enfants et adultes, "
+                    "femme et homme.",)
+          ])),
+    ],),
+  );
 }
