bb0c4127f90dc99c006cad198676fd5126a3cc0f
elsone <elsanal1995@gmail.com>
2022-03-10 21:28:26 +0800

    Update upload_post.dart

--- lib/MainPages/Account/MyShop/upload_post.dart
+++ lib/MainPages/Account/MyShop/upload_post.dart
@@ -2,6 +2,7 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:openshop/Decorations/deco_search_bar.dart';
+import 'package:openshop/Decorations/text_deco.dart';
 
 class UploadPost extends StatefulWidget {
   const UploadPost({Key? key}) : super(key: key);
@@ -19,7 +20,12 @@ class _UploadPostState extends State<UploadPost> {
   String? selectedValue;
   // late List<File> photos;
   // late File video;
-  List<String> category = ["Clothes","Electronic"];
+  List<String> category = ["Clothes","Electronics","Shoes","Furniture","Toys",
+    "Gadgets","Foods","Drinks","Books","Construction",
+    "Automobile","Animal","Renting"];
+  final _formKey = GlobalKey<FormState>();
+
+  
 
   @override
   void initState() {
@@ -28,29 +34,36 @@ class _UploadPostState extends State<UploadPost> {
   }
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.black,
       body: Container(
         margin: const EdgeInsets.only(left: 5, right: 5),
+        child: Form(
 
-        child: ListView(children: [
+          child: ListView(children: [
           Container(
-            color: Colors.white,
             padding: const EdgeInsets.only(top: 5, bottom: 5),
+            decoration: const BoxDecoration(color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 const Text("Tap to select photos"),
                 ElevatedButton(
-                onPressed: (){},
-                child: const Icon(Icons.photo_library_rounded, size: 100,color: Colors.black,),
-              ),
+                  onPressed: (){},
+                  child: const Icon(Icons.photo_library_rounded, size: 100,color: Colors.black,),
+                ),
               ],
             ),),
           const SizedBox(height: 25,),
           Container(
-            color: Colors.white,
             padding: const EdgeInsets.only(top: 5, bottom: 5),
+            decoration: const BoxDecoration(color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -68,7 +81,20 @@ class _UploadPostState extends State<UploadPost> {
             autocorrect: true,
             autofocus: true,
             maxLines: 1,
-            decoration: uploadFieldDeco.copyWith(hintText: "Enter title"),
+            decoration: uploadFieldDeco.copyWith(hintText: "Enter a description of the product"),
+            onChanged:(String? value){title = value!;},
+          ),
+          const SizedBox(height: 25,),
+          _dropDown(height, width),
+          const SizedBox(height: 25,),
+          TextFormField(
+            enableSuggestions: true,
+            enabled: true,
+            autocorrect: true,
+            autofocus: true,
+            maxLines: 5,
+            toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
+            decoration: uploadFieldDeco.copyWith(hintText: "Most important characteristics of the product"),
             onChanged:(String? value){title = value!;},
           ),
           const SizedBox(height: 25,),
@@ -79,7 +105,7 @@ class _UploadPostState extends State<UploadPost> {
             autofocus: true,
             maxLines: 5,
             toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
-            decoration: uploadFieldDeco.copyWith(hintText: "Describe the products"),
+            decoration: uploadFieldDeco.copyWith(hintText: "Add some tags to make your product more searchable"),
             onChanged:(String? value){title = value!;},
           ),
           const SizedBox(height: 25,),
@@ -88,43 +114,21 @@ class _UploadPostState extends State<UploadPost> {
             enabled: true,
             autocorrect: true,
             autofocus: true,
-            maxLines: 5,
-            toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
-            decoration: uploadFieldDeco.copyWith(hintText: "Most important characteristics"),
-            onChanged:(String? value){title = value!;},
-          ),
-          const SizedBox(height: 25,),
-          TextFormField(
-            enableSuggestions: true,
-            enabled: true,
-            autocorrect: true,
-            autofocus: true,
-            maxLines: 5,
-            toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
-            decoration: uploadFieldDeco.copyWith(hintText: "Input tags"),
-            onChanged:(String? value){title = value!;},
-          ),
-          const SizedBox(height: 25,),
-          TextFormField(
-            enableSuggestions: true,
-            enabled: true,
-            autocorrect: true,
-            autofocus: true,
-            maxLines: 5,
+            maxLines: 1,
             toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
             decoration: uploadFieldDeco.copyWith(hintText: "Original price"),
             onChanged:(String? value){title = value!;},
           ),
-          _dropDown(),
           const SizedBox(height: 25,),
           TextFormField(
             enableSuggestions: true,
             enabled: true,
             autocorrect: true,
             autofocus: true,
-            maxLines: 5,
+            maxLines: 1,
             toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
-            decoration: uploadFieldDeco.copyWith(hintText: "Actual price"),
+            decoration: uploadFieldDeco.copyWith(hintText: "If there is a discount now, "
+                "please enter the actual price. If not enter the original price"),
             onChanged:(String? value){title = value!;},
           ),
           const SizedBox(height: 25,),
@@ -133,9 +137,9 @@ class _UploadPostState extends State<UploadPost> {
             enabled: true,
             autocorrect: true,
             autofocus: true,
-            maxLines: 5,
+            maxLines: 1,
             toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
-            decoration: uploadFieldDeco.copyWith(hintText: "quantity"),
+            decoration: uploadFieldDeco.copyWith(hintText: "How many products are on sell?"),
             onChanged:(String? value){title = value!;},
           ),
           const SizedBox(height: 25,),
@@ -144,62 +148,44 @@ class _UploadPostState extends State<UploadPost> {
             enabled: true,
             autocorrect: true,
             autofocus: true,
-            maxLines: 5,
+            maxLines: 1,
             toolbarOptions: const ToolbarOptions(copy: true,cut: true,paste: true,selectAll: true),
             decoration: uploadFieldDeco.copyWith(hintText: "Minimum for order"),
             onChanged:(String? value){title = value!;},
           ),
-          Text(""),
-          Text(""),
-          Text(""),
-          Text("")
-        ],),
+          const SizedBox(height: 25,),
+          ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepOrangeAccent)),
+              onPressed: (){},
+              child: const Text("Tap to upload online")),
+          const SizedBox(height: 50,),
+        ],),),
       ),
     );
   }
 
-  Widget _dropDown(){
+  Widget _dropDown(double height, double width){
     return Center(
       child: DropdownButtonHideUnderline(
         child: DropdownButton2(
           isExpanded: true,
-          hint: Row(
-            children: const [
-              Icon(
-                Icons.list,
-                size: 16,
-                color: Colors.yellow,
-              ),
-              SizedBox(
-                width: 4,
-              ),
-              Expanded(
-                child: Text(
-                  'Select Item',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.yellow,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
+          hint: Text(
+            'Tap to select product category',
+            style: style,
+            overflow: TextOverflow.ellipsis,
           ),
           items: category
               .map((item) => DropdownMenuItem<String>(
             value: item,
-            child: Text(
-              item,
-              style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            child: ListTile(
+              leading: Text(
+                item,
+                style: style,
+                overflow: TextOverflow.ellipsis,
               ),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ))
-              .toList(),
+            )
+          )).toList(),
           value: selectedValue,
           onChanged: (value) {
             setState(() {
@@ -208,35 +194,36 @@ class _UploadPostState extends State<UploadPost> {
           },
           icon: const Icon(
             Icons.arrow_forward_ios_outlined,
+            color: Colors.black,
           ),
           iconSize: 14,
           iconEnabledColor: Colors.yellow,
           iconDisabledColor: Colors.grey,
-          buttonHeight: 50,
-          buttonWidth: 160,
+          // buttonHeight: 50,
+          // buttonWidth: 160,
           buttonPadding: const EdgeInsets.only(left: 14, right: 14),
           buttonDecoration: BoxDecoration(
             borderRadius: BorderRadius.circular(14),
             border: Border.all(
-              color: Colors.black26,
+              color: Colors.orange,
             ),
-            color: Colors.redAccent,
+            color: Colors.white,
           ),
-          buttonElevation: 2,
+          buttonElevation: 0,
           itemHeight: 40,
           itemPadding: const EdgeInsets.only(left: 14, right: 14),
-          dropdownMaxHeight: 200,
-          dropdownWidth: 200,
-          dropdownPadding: null,
+          dropdownMaxHeight: height/2,
+          dropdownWidth: width*(2/3),
+          dropdownPadding:null,
           dropdownDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(14),
-            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
           ),
-          dropdownElevation: 8,
+          dropdownElevation: 5,
           scrollbarRadius: const Radius.circular(40),
           scrollbarThickness: 6,
           scrollbarAlwaysShow: true,
-          offset: const Offset(-20, 0),
+          offset: const Offset(20, 3),
         ),
       ),
     );
